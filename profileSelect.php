26996be9f70985892c5f1155a2a86cc923bd752d
amckay13 <amckay13@terpmail.umd.edu>
2016-04-18 18:16:40 -0400

    Added more pretty Bootstrap

--- profileSelect.php
+++ profileSelect.php
@@ -2,27 +2,35 @@
     <head> 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
         <title>Profile Select Page</title>
-        <link rel="stylesheet" href="mainstyle.css" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body>
-		<form action="navigating.php" method="post">
+		<a class="btn btn-info" href="mainPage.html" role="button">Back to Main</a>
+		<!--<form action="navigating.php" method="post">
 			<input type="submit" name="back_landing" style="width:50px; height:35px;" value="Back to Title" /><br />
 			<p>&nbsp;</p>
-		</form>	
-        <header class="header">
-            <h1>Storybook Login</h1>
-        </header>    
-        <section class="main">         
-            <p>&nbsp;</p>
-			<form action="navigating.php" method="post">
-				<input type="submit" name="profile1" style="width:300px; height:80px;" value="Anya" /><br />
-                <p>&nbsp;</p>
-                <input type="submit" name="profile2" style="width:300px; height:80px;" value="Brooklyn" /><br />
-                <p>&nbsp;</p>
-				<input type="submit" name="profile3" style="width:300px; height:80px;" value="Parents" /><br />
-                <p>&nbsp;</p>
-			</form>		
-        </section>
+		</form>	-->
+		<div class="container text-center">
+			<header class="header">
+				<h1>Storybook Login</h1>
+			</header>
+		</div>
+		
+		 <div class="container text-center">
+			<section class="main">         
+				<p>&nbsp;</p>
+				<form action="navigating.php" method="post">
+					<input type="submit" name="profile1" class="btn btn-primary" value="Anya" style="width:300px; height:80px;"/><br />
+					<p>&nbsp;</p>
+					<input type="submit" name="profile2" class="btn btn-primary" value="Brooklyn" style="width:300px; height:80px;"/><br />
+					<p>&nbsp;</p>
+					<input type="submit" name="profile3" class="btn btn-primary" value="Parents" style="width:300px; height:80px;"/><br />
+					<p>&nbsp;</p>
+				</form>		
+			</section>
+		</div>
+		 
+		 <script src="js/bootstrap.min.js"></script>
     </body>  
 </html>

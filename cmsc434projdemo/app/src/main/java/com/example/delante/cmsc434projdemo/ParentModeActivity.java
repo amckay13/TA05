d69f3f724d782f9f0882637fe4b9bc5b5f97792a
nr92cp <derek.ferrell@ymail.com>
2016-05-03 14:03:50 -0400

    Created parent settings/info pages

--- cmsc434projdemo/app/src/main/java/com/example/delante/cmsc434projdemo/ParentModeActivity.java
+++ cmsc434projdemo/app/src/main/java/com/example/delante/cmsc434projdemo/ParentModeActivity.java
@@ -1,7 +1,10 @@
 package com.example.delante.cmsc434projdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ParentModeActivity extends AppCompatActivity {
 
@@ -9,6 +12,36 @@ public class ParentModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parentpage);
+
+        final Button settingsButton =  (Button) findViewById(R.id.buttonSettings);
+        final Button storeButton =  (Button) findViewById(R.id.buttonStore);
+        final Button infoButton =  (Button) findViewById(R.id.buttonInfo);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ParentModeActivity.this, ParentSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*storeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ParentModeActivity.this, ParentStoreActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ParentModeActivity.this, ParentInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }

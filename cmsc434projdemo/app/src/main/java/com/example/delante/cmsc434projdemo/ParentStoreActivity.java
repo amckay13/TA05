fbcc8191d6e10030bf9e3b900cbb15e2c403510a
nr92cp <derek.ferrell@ymail.com>
2016-05-03 18:11:11 -0400

    Added books to store

--- cmsc434projdemo/app/src/main/java/com/example/delante/cmsc434projdemo/ParentStoreActivity.java
+++ cmsc434projdemo/app/src/main/java/com/example/delante/cmsc434projdemo/ParentStoreActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -16,9 +17,23 @@ public class ParentStoreActivity extends AppCompatActivity {
         setContentView(R.layout.parent_store_page);
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        ImageView imageView2 = (ImageView) findViewById(R.id.imageView2);
+        ImageView imageView3 = (ImageView) findViewById(R.id.imageView3);
 
-        Picasso.with(this)
-                .load("http://vignette4.wikia.nocookie.net/mrmen/images/5/52/Small.gif/revision/latest?cb=20100731114437")
-                .into(imageView);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.jackbean));
+        imageView2.setImageDrawable(getResources().getDrawable(R.drawable.littlered));
+        imageView3.setImageDrawable(getResources().getDrawable(R.drawable.littlepigs));
+    }
+
+    public void buyBeanstalk(View view) {
+        Toast.makeText(this, "You have bought 'Jack and the Beanstalk'", Toast.LENGTH_SHORT).show();
+    }
+
+    public void buyLittleRed(View view) {
+        Toast.makeText(this, "You have bought 'Little Red Riding Hood'", Toast.LENGTH_SHORT).show();
+    }
+
+    public void buyThreeBears(View view) {
+        Toast.makeText(this, "You have bought 'You have bought 'The Three Little Bears'", Toast.LENGTH_SHORT).show();
     }
 }
